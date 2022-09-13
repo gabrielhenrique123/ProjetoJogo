ff6384d3ca6f9444c7a87ca9f64ac3cd724c7e49
gabrielhenrique123 <gabycristoffano@gmail.com>
2022-09-13 08:27:33 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrjJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
